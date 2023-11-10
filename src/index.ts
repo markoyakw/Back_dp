a6a75c8b85ce1d8acd15762e1a9eff8b9bd00ec8
mrkykvnk <128623454+mrkykvnk@users.noreply.github.com>
2023-11-10 18:49:17 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import cors from "cors"
 const PORT = process.env.PORT || 5000
 const app = express();
 app.use(cors({
-    origin: 'https://yakovenkodiploma.vercel.app/',
+    origin: 'https://yakovenkodiploma.vercel.app/:1',
     credentials: true
 }))
 app.use(express.json())
