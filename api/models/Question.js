2a2f6fb1d9d0eb5c07341d8e121d8404117dc899
mrkykvnk <yakovenkomarko@gmail.com>
2024-11-20 21:02:14 +0100

    Vercel fix

--- api/models/Question.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const mongoose_2 = require("mongoose");
-const Question = new mongoose_2.Schema();
-exports.default = (0, mongoose_1.model)("Question", Question);
-//# sourceMappingURL=Question.js.map
